a19f0ee4dd970e69d17d03c23ca0a64143e881f9
Abigail Nacional <aan16@duke.edu>
2022-03-13 09:48:19 -0400

    Modified pages, added location page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 #Imports
-from flask import Flask, render_template
+from urllib import response
+from flask import Flask, render_template, redirect, url_for
 from appmodels import create_app
 import os
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy, request
 import sqlalchemy.orm
 from flask_googlemaps import GoogleMaps
 import requests
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, NumberRange, Length
+from flask_babel import _, lazy_gettext as _l
 
 #Create app using function
 app = create_app()
@@ -26,16 +31,30 @@ def index():
 def tutorial():
      return render_template("tutorial.html")
 
-#Directs user to findhhw page
+class LocForm(FlaskForm):
+    location = StringField(_l('Location'), validators=[DataRequired()])
+    submit = SubmitField(_l('Sign In'))
+
+#Directs user to findhhw page, allows users to search a location
 @app.route("/findhhw")
 def findhhw():
+     #form = LocForm()
+     #if form.validate_on_submit():
+     #     location = form.location.data
+     #     return render_template("locatehhw.html", location=location)
+     #else:
+          return render_template("findhhw.html")
+
+#Displays Google Map to nearest HHW locations based on search
+@app.route("/locatehhw")
+def locatehhw():
      #Add location bias later
      url = "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=household%20hazardous%20waste&inputtype=textquery&fields=formatted_address%2Cname%2Cphoto&key=AIzaSyC5wxVkXV3QR6DBt_Rz2-vJ395qU_Prr-I"
      payload={}
      headers = {}
 
      response = requests.request("GET", url, headers=headers, data=payload)
-     return render_template("findhhw.html", response=response)
+     return render_template("locatehhw.html", response=response)
 
 if __name__ == "__main__":
   app.run()
